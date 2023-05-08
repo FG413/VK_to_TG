6f0bffba2f99c948ae1bd017e3f23f3a80a77bf6
FG413 <129945809+FG413@users.noreply.github.com>
2023-05-08 20:35:23 +0300

    Add files via upload

--- src/main/java/com/example/NL_MessageBot/service/VkData.java
+++ src/main/java/com/example/NL_MessageBot/service/VkData.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
@@ -22,11 +23,11 @@ public class VkData {
     static String history="";
     public static String dataReader(UserActor actor) throws ClientException, ApiException {
 
-        List<Integer> conId =vk.messages().getConversations(actor).execute().getItems().stream().map(ConversationWithMessage::getConversation).map(Conversation::getPeer).map(ConversationPeer::getId).toList();
-        List<Integer> ConUnread= vk.messages().getConversations(actor).execute().getItems().stream().map(ConversationWithMessage::getConversation).map(Conversation::getUnreadCount).toList();
+        List<Integer> conId =vk.messages().getConversations(actor).execute().getItems().stream().map(ConversationWithMessage::getConversation).map(Conversation::getPeer).map(ConversationPeer::getId).collect(Collectors.toList());
+        List<Integer> ConUnread= vk.messages().getConversations(actor).execute().getItems().stream().map(ConversationWithMessage::getConversation).map(Conversation::getUnreadCount).collect(Collectors.toList());
         for(int count = 0; count <20; count++) {
         if (ConUnread.get(count)!=null){
-            history =history + vk.messages().getHistory(actor).userId(conId.get(count)).count(ConUnread.get(count)).execute().getItems().stream().map(Message::getText).toList().toString();
+            history =history + vk.messages().getHistory(actor).userId(conId.get(count)).count(ConUnread.get(count)).execute().getItems().stream().map(Message::getText).collect(Collectors.toList()).toString();
         }
         }
         String subHistory=history;
